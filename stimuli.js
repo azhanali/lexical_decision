7d52281ca48456360de3d9993218853f686a6a1d
Azhan <43489763+azhanali@users.noreply.github.com>
2021-05-19 23:50:22 +0530

    Add files via upload

---
+++ stimuli.js
@@ -0,0 +1,70 @@
+var test_stimuli = [
+  {
+    "target": "sentence",
+    "condition": "word"
+  },
+  {
+    "target": "ocean",
+    "condition": "word"
+  },
+  {
+    "target": "monkey",
+    "condition": "word"
+  },
+  {
+    "target": "fright",
+    "condition": "word"
+  },
+  {
+    "target": "summary",
+    "condition": "word"
+  },
+  {
+    "target": "strength",
+    "condition": "word"
+  },
+  {
+    "target": "carpet",
+    "condition": "word"
+  },
+  {
+    "target": "lobster",
+    "condition": "word"
+  },
+  {
+    "target": "table",
+    "condition": "word"
+  },
+  {
+    "target": "tarl",
+    "condition": "nonword"
+  },
+  {
+    "target": "selephore",
+    "condition": "nonword"
+  },
+  {
+    "target": "fent",
+    "condition": "nonword"
+  },
+  {
+    "target": "coe",
+    "condition": "nonword"
+  },
+  {
+    "target": "gine",
+    "condition": "nonword"
+  },
+  {
+    "target": "woather",
+    "condition": "nonword"
+  },
+  {
+    "target": "wenner",
+    "condition": "nonword"
+  },
+  {
+    "target": "zofe",
+    "condition": "nonword"
+  }
+]
